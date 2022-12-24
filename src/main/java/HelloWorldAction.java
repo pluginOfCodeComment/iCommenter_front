8fff29fb3c05bc63b8c732e8ba9f344924e0646e
qiuqiu <2274572823@qq.com>
2022-12-24 17:00:54 +0800

    init

---
+++ src/main/java/HelloWorldAction.java
@@ -0,0 +1,24 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.psi.PsiFile;
+
+public class HelloWorldAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        Project project = e.getData(PlatformDataKeys.PROJECT);
+
+        //获取当前操作的类文件
+        PsiFile psiFile = e.getData(CommonDataKeys.PSI_FILE);
+        //获取当前类文件的路径
+        String classPath = psiFile.getVirtualFile().getPath();
+        String title = "Hello World!";
+
+        Messages.showMessageDialog(project, classPath, title, Messages.getInformationIcon());
+        // TODO: insert action logic here
+    }
+}
---
+++ src/main/java/RS.java
@@ -0,0 +1,13 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.SelectionModel;
+
+public class RS extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+
+        // TODO: insert action logic here
+    }
+}
